06dec9d45b74ca0d8eb0288b6eb8695bd8c25b93
andrey73m <andrey12mora@gmail.com>
2024-04-23 18:07:33 -0500

    Formulario login

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
+import FormularioLogin from "./components/formularios/login"
 import FormularioRegistrarse from "./components/formularios/registrarse"
 const App = () => {
   
   return (
     <div className="w-screen h-screen bg-[#1E0132] text-white">
       <div className="w-full  h-full flex items-center justify-center">
-        <FormularioRegistrarse/>
+        {/*<FormularioRegistrarse/>*/}
+        {/*<FormularioLogin/>*/}
       </div>
     </div>
   )
