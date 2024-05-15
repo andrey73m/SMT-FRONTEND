19fb8d9d0f90e5a96647b2fc27727719a88fd1ad
CarlosDRomero <cdavid.romeror@gmail.com>
2024-05-15 18:58:11 -0500

    arreglos en el hook de rol

--- src/hooks/validacionRol.ts
+++ src/hooks/validacionRol.ts
@@ -1,4 +1,5 @@
 import useRolUsuario from "./rol"
+import useSesion from "./sesion";
 
 export interface opcionesValidacionRol{
   roles?: string[];
@@ -9,12 +10,13 @@ export interface opcionesValidacionRol{
 
 const useValidacionRol = ({ roles, permitirSinAutenticar, esperarRol = true, or }:opcionesValidacionRol) => {
   const { rol, isFetching } = useRolUsuario()
+  const { haySesion } = useSesion()
   return {
     rol,
     isFetching,
     valido:
       or ||
-      ((!roles && !permitirSinAutenticar && rol)
+      ((!roles && haySesion && !permitirSinAutenticar)
       || (roles && roles.includes(rol))
       || (permitirSinAutenticar && !esperarRol && !rol)
       || (permitirSinAutenticar && !isFetching && !rol))
