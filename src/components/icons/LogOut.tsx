50c39cdae647d504c40d4ac724e0f58818fa71d4
andrey73m <andrey12mora@gmail.com>
2024-05-20 18:19:57 -0500

    arreglo iconos

--- src/components/icons/LogOut.tsx
+++ src/components/icons/LogOut.tsx
@@ -6,7 +6,7 @@ interface Props extends React.SVGAttributes<HTMLOrSVGElement>{
 
 const LogOut = ({ className, ping,...props }: Props) => {
   return (
-    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" {...props} className={cn("h-full", className,{
+    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" {...props} className={cn("h-full w-full", className,{
       "animate-ping": ping
     })}>
       <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 9V5.25A2.25 2.25 0 0 0 13.5 3h-6a2.25 2.25 0 0 0-2.25 2.25v13.5A2.25 2.25 0 0 0 7.5 21h6a2.25 2.25 0 0 0 2.25-2.25V15m3 0 3-3m0 0-3-3m3 3H9" />
