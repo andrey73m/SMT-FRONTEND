aa4d3244bd7eeaa06c844898ce1cc8937a5b0d15
andrey73m <andrey12mora@gmail.com>
2024-10-21 14:51:17 -0500

    Se modificaron estilos de scroll y botón

--- src/components/UI/Botones/BotonPrimario.tsx
+++ src/components/UI/Botones/BotonPrimario.tsx
@@ -5,7 +5,7 @@ import Boton, { BotonProps } from "./Boton";
  
 const BotonPrimario = ({ className, negar,...props }: BotonProps) => {
   return (
-    <Boton {...props} className={cn("bg-violet-700 enabled:hover:bg-violet-500",{
+    <Boton {...props} className={cn("bg-violet-700 enabled:hover:bg-violet-500 disabled:bg-violet-400",{
       "bg-slate-200 enabled:hover:bg-slate-300 text-violet-700":negar
     }, className)}/>
   );
