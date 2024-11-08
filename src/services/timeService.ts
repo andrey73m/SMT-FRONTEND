a2abe05abcbe36bf92f83464df5ab2b2435d8d2c
CarlosDRomero <cdavid.romeror@gmail.com>
2024-11-08 08:10:15 -0500

    Add function for convert a date to a ISO string date

--- src/services/timeService.ts
+++ src/services/timeService.ts
@@ -3,5 +3,14 @@ import moment from "moment"
 export default {
   convertirFechaEnIntervalo: (fecha: Date) => {
     return moment(fecha).fromNow()
+  },
+  convertirFechaEnIso: (fecha?: string) => {
+    if (fecha)
+      try{
+        const fechaIso = new Date(fecha).toISOString().split("T")[0]
+        if (fechaIso !== "Invalid Date") return fechaIso;
+      }catch(e){/**/}
+
+    return ""
   }
 }
