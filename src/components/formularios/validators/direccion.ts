8e156eef5aeb7582cd721256c5c2fcbff3e4ccc1
CarlosDRomero <cdavid.romeror@gmail.com>
2025-11-12 13:02:20 -0500

    update c_dane_municipio validation to new regex

--- src/components/formularios/validators/direccion.ts
+++ src/components/formularios/validators/direccion.ts
@@ -4,7 +4,7 @@ import { z } from "zod"
 
 export const direccionValidator = z.object({
   c_dane_departamento: z.string().regex(/^\d+$/, "Escoge un departamento valido"),
-  c_dane_municipio: z.string().regex(/^\d+(\.\d+)?$/, "Escoge un municipio valido"),
+  c_dane_municipio: z.string().regex(/^(\d+)-(\d+)$/, "Escoge un municipio valido"),
   barrio: z.string().min(5, "Ingresa un barrio valido"),
   cadena_direccion: z.string().min(8, "La direccion debe ser mayor a 8 caracteres"),
   predeterminada: z.boolean({ message:"A" })
