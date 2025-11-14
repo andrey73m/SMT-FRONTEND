e75005d0b4ca2961c5bbcf6a6203a53951b41fdf
CarlosDRomero <cdavid.romeror@gmail.com>
2025-11-14 10:43:22 -0500

    add RAEE quantity info

--- src/components/pages/Recicla.tsx
+++ src/components/pages/Recicla.tsx
@@ -1,3 +1,4 @@
+import { useQueryRegistrosRAEE } from "@/hooks/raee";
 import FormularioRAEE from "../formularios/raee";
 import AlternarFormularioSticky from "../layout/AlternarFormularioSticky";
 import { ImagenTitulo } from "../layout/FormatoImagenes";
@@ -5,11 +6,18 @@ import { BotonPositivo } from "../UI/Botones";
 import ListaRAEE from "../views/ambiental/ListaRAEE";
 
 const PaginaRAEE = () => {
+  const {data} = useQueryRegistrosRAEE()
+  const cantidad = data?.length;
   return(
     <>
       <ImagenTitulo titulo="Disposición de RAEE" urlImagen="https://ecocomputo.com/wp-content/uploads/2025/09/que-hacer-con-los-raee-en-colombia-banda-transporte-operarios.jpg"/>
       <AlternarFormularioSticky texto="¿Tienes aparatos eléctricos o electrónicos viejos y no sabes como desecharlos correctamente?" Formulario={FormularioRAEE} Boton={BotonPositivo}/>
-      <ListaRAEE/>      
+      <section className="flex flex-col items-center my-2 text-2xl border-t-2 py-4 border-b-2">
+
+      <p className="">¡Forma parte!, ya se han registrado <span className="font-bold bg-green-500 px-3 py-1  rounded-full text-white">{cantidad}</span> dipositivos</p>
+      <BotonPositivo className=" underline rounded-none mt-4" simplificar={true}  negar={true}>¿Por qué participar?</BotonPositivo>
+      </section>
+      <ListaRAEE/>
     </>
   )
 }
